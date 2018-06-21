42efd8d6c94821c74a3302ab555a10897e7e49ab
Pasante <Pasante@GLB-L1099.globant.com>
2018-06-21 13:28:28 -0300

    hola

---
+++ celu.js
@@ -0,0 +1,48 @@
+var i1= document.getElementById("sn")
+var i2= document.getElementById("in")
+var i3= document.getElementById("fa")
+var i4= document.getElementById("wh")
+var i5= document.getElementById("bat")
+var i6= document.getElementById("tw")
+var i7= document.getElementById("te")
+var i8= document.getElementById("it")
+var i9=document.getElementById("cl")
+var i10=document.getElementById("vs")
+var i11=document.getElementById("re")
+var i12=document.getElementById("sa")
+i1.addEventListener ("click", function(){
+    window.location.href="https://www.google.com.ar/search?q=snapcja&oq=snapcja&aqs=chrome..69i57j0l5.1682j0j7&sourceid=chrome&ie=UTF-8"
+});
+i2.addEventListener ("click", function(){
+    window.location.href="https://www.instagram.com/?hl=es-la"
+});
+i3.addEventListener ("click", function(){
+    window.location.href="https://es-la.facebook.com/"
+});
+i4.addEventListener ("click", function(){
+    window.location.href="https://web.whatsapp.com/%F0%9F%8C%90/es"
+});
+i5.addEventListener ("click", function(){
+    window.location.href="file:///C:/Users/Pasante/bateria/bateria.html"
+});
+i6.addEventListener ("click", function(){
+    window.location.href="https://twitter.com/iniciarsesion?lang=es"
+});
+i7.addEventListener ("click", function(){
+    window.location.href=""
+});
+i8.addEventListener ("click", function(){
+    window.location.href="https://www.apple.com/la/itunes/download/"
+});
+i9.addEventListener ("click", function(){
+    window.location.href="https://www.google.com.ar/search?q=clima&oq=clima&aqs=chrome..69i57j69i60j5j69i60l3.1894j0j4&sourceid=chrome&ie=UTF-8"
+});
+i10.addEventListener ("click", function(){
+    window.location.href="https://vsco.co/user/login"
+});
+i11.addEventListener ("click", function(){
+    window.location.href="https://24timezones.com/es_hora/argentina_reloj.php"
+});
+i12.addEventListener ("click", function(){
+    window.location.href="https://www.apple.com/la/safari/"
+});
